673ab96e7f853d587fc7c48d4128ada420419b05
Elili <elili.sivam@gmail.com>
2024-12-03 22:30:56 -0500

    dded MIT license

--- popup.js
+++ popup.js
@@ -1,3 +1,12 @@
+/*!
+ * MIT License
+ * Copyright (c) 2024 Lexi
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction...
+ */
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Loading spinner functions
